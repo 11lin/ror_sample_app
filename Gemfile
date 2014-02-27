f2c78df167445699b450306dd4abda1e80792c51
exit <irb>
2014-02-27 19:13:33 +0800

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '4.0.3'
 group :development,:test do
 gem 'sqlite3'
 gem 'rspec-rails'
+gem 'guard-rspec'
 end
 
 group :test do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,16 +13,21 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+	let(:spec_base_title){"Ruby on Rails Tutorial Sample App"}
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-    it "should have the title 'Home'" do
-	visit '/static_pages/home'
-	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-end
+  describe "Home page" do
+	it "should have the content 'Sample App'" do
+		visit '/static_pages/home'
+		expect(page).to have_content('Sample App')
+	end
+	it "should have the title 'Home'" do
+		visit '/static_pages/home'
+		expect(page).to have_title("#{spec_base_title}")
+	end
+	it "should not have a custom page title" do
+		visit '/static_pages/home'
+		expect(page).not_to have_title('| Home')
+	end
   end
 
 describe "Help page" do
@@ -32,7 +37,7 @@ describe "Help page" do
 	end
 	it "should have the title 'Help'" do
 		visit '/static_pages/help'
-		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		expect(page).to have_title("#{spec_base_title} | Help")
 	end
 end
 
@@ -43,7 +48,17 @@ describe "About page" do
 	end
 	it "should have the title 'About Us'" do
 		visit '/static_pages/about'
-		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+		expect(page).to have_title("#{spec_base_title} | About Us")
+	end
+end
+describe "Contact page" do
+	it "should have the content 'Contact'" do
+		visit '/static_pages/contact'
+		expect(page).to have_content("Contact")
+	end
+	it "should have the title 'Contact'" do
+		visit '/static_pages/contact'
+		expect(page).to have_title("#{spec_base_title} | Contact")
 	end
 end
 
