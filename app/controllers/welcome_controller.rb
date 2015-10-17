87751b08df0f862c8fca479cfb31151155993169
jianlin <jcfss@126.com>
2015-10-17 21:15:05 +0800

    rails demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+	respond_to do |format|  
+		format.html # show.html.erb  
+		format.xml  { render :xml => "xml" } 
+		format.json { render :json => "json" }    
+	end
   end
 end
---
+++ test.rb
@@ -0,0 +1,13 @@
+class Test
+	def new
+
+	end
+	def create
+		puts "article_params",article_params
+	end
+	def article_params
+		"ss"
+	end
+end
+t=Test.new
+t.create
